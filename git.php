def2f737aba1fc6755a06b81de130db595c90bcf
Daniil <67641369+Daniil321123@users.noreply.github.com>
2021-02-20 21:33:00 +0200

    Added js functional

--- git.php
+++ git.php
@@ -1,2 +1,14 @@
-<?php
-var_dump(123123);die;
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test</title>
+    <script src="training.js"></script>
+</head>
+<body>
+
+</body>
+</html>
