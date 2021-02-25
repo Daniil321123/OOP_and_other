e1c5b003f8af71b57bdd32f628722090667c0608
Daniil <67641369+Daniil321123@users.noreply.github.com>
2021-02-25 12:22:29 +0200

    Added html

--- git.php
+++ git.php
@@ -1,6 +1,18 @@
-<<<<<<< HEAD
 <?php
 $text =  'aba accca azzza wwwwa';
 echo   $text. '<br>';
 echo preg_replace("/a.+?a/", '!',   $text);
 ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
